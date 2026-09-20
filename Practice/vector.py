8f4ed531895ca22884f6dd4b3ee53e7b7adc4589
yeabsira-asaminew <133232295+yeabsira-asaminew@users.noreply.github.com>
2026-09-20 09:16:27 +0000

    Practice notebook for intro

---
+++ Practice/vector.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+u = np.array([
+    [1, 0, 3],
+    [2, 3, 5],
+    [1, 3, 0]
+])
+
+v = np.array([
+    [1, 0],
+    [3, 1],
+    [1, 1]
+])
+
+def vector_multip(u, v):
+    result = u.dot(v)
+    
+    return result
+
+def dot_multiplication(u, v):
+    assert u.shape[1] == v.shape[0]
+    # why u.shape[] is shape function?
+    rows_u = u.shape[0] 
+    rows_v = v.shape[0]
+    columns_u = u.shape[1]
+    columns_v = v.shape[1]
+    
+    result = np.zeros((rows_u, columns_v))
+    
+    for i in range(rows_u):
+        for j in range(columns_v):
+            for k in range(columns_u):
+                
+                result[i, j] = result[i, j] + u[i, k] * v[k, j]
+                
+    return result
+
+
+if np.array_equal(dot_multiplication(u, v), vector_multip(u, v)):
+    print("Hello world!")
+else:
+    print("Hiiiiii")
+                
+                
